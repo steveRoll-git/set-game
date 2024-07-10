3ebfac393d92fcf53e85a3bde7561e63da339d90
Ron Tseytlin <ronts390@gmail.com>
2024-07-10 20:37:07 +0300

    CardSprite - added a slight shadow

--- src/CardSprite.ts
+++ src/CardSprite.ts
@@ -1,4 +1,4 @@
-import { Container, Graphics, Sprite } from "pixi.js"
+import { BlurFilter, Container, Graphics, Sprite } from "pixi.js"
 import { Card } from "./main"
 import { getSymbolTexture } from "./symbolImages"
 
@@ -11,8 +11,14 @@ export class CardSprite extends Container {
   constructor(card: Card) {
     super()
 
+    const shadow = new Graphics()
+    shadow.roundRect(0, 0, cardWidth, cardHeight, 12).fill("00000044")
+    shadow.filters = [new BlurFilter({ strength: 2 })]
+    this.addChild(shadow)
+
     const graphics = new Graphics()
     graphics.roundRect(0, 0, cardWidth, cardHeight, 12).fill(0xffffff)
+    this.addChild(graphics)
 
     const symbolContainer = new Container()
     for (let i = 0; i <= card[0]; i++) {
@@ -21,8 +27,6 @@ export class CardSprite extends Container {
       symbolContainer.addChild(symbolSprite)
     }
     symbolContainer.tint = colors[card[1]]
-
-    this.addChild(graphics)
     this.addChild(symbolContainer)
     symbolContainer.x = this.width / 2 - symbolContainer.width / 2
     symbolContainer.y = this.height / 2 - symbolContainer.height / 2
